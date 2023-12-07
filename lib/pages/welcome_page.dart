53c109f1f10dfe01817e38ac79127430f6e60b86
AyaIbrahim3027 <ayaibrahim3027@gmail.com>
2023-12-07 06:53:06 +0200

    first commit

---
+++ lib/pages/welcome_page.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class WelcomePage extends StatefulWidget {
+  const WelcomePage({super.key});
+
+  @override
+  State<WelcomePage> createState() => _WelcomePageState();
+}
+
+class _WelcomePageState extends State<WelcomePage> {
+  List images = [
+    'assets/welcome_img1.png',
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageView.builder(
+          scrollDirection: Axis.vertical,
+          itemCount: images.length,
+          itemBuilder: (_, index) {
+            return Container(
+              width: double.maxFinite,
+              height: double.maxFinite,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage(
+                    images[index],
+                  ),
+                ),
+              ),
+            );
+          }),
+    );
+  }
+}
