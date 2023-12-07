947b09b66f79d7f454cc6e284c37da212dc3d4b9
AyaIbrahim3027 <ayaibrahim3027@gmail.com>
2023-12-07 09:23:52 +0200

    create welcome page

--- lib/pages/welcome_page.dart
+++ lib/pages/welcome_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:travel_app/misc/colors.dart';
+import 'package:travel_app/widgets/app_large_text.dart';
+import 'package:travel_app/widgets/app_text.dart';
+import 'package:travel_app/widgets/responsive_button.dart';
 
 class WelcomePage extends StatefulWidget {
   const WelcomePage({super.key});
@@ -9,7 +13,10 @@ class WelcomePage extends StatefulWidget {
 
 class _WelcomePageState extends State<WelcomePage> {
   List images = [
-    'assets/welcome_img1.png',
+    'assets/welcome_img3.jpg',
+    'assets/welcome_img1.jpg',
+    // 'assets/welcome_img2.jpg',
+    'assets/welcome_img4.jpg',
   ];
   @override
   Widget build(BuildContext context) {
@@ -17,7 +24,7 @@ class _WelcomePageState extends State<WelcomePage> {
       body: PageView.builder(
           scrollDirection: Axis.vertical,
           itemCount: images.length,
-          itemBuilder: (_, index) {
+          itemBuilder: (context, index) {
             return Container(
               width: double.maxFinite,
               height: double.maxFinite,
@@ -26,6 +33,62 @@ class _WelcomePageState extends State<WelcomePage> {
                   image: AssetImage(
                     images[index],
                   ),
+                  fit: BoxFit.cover,
+                ),
+              ),
+              child: Container(
+                margin: const EdgeInsets.only(
+                  top: 150,
+                  left: 20,
+                  right: 20,
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        AppLargeText(text: 'Trips'),
+                        AppText(
+                          text: 'Mountain',
+                          size: 30,
+                        ),
+                        SizedBox(
+                          height: 13,
+                        ),
+                        Container(
+                          width: 250,
+                          child: AppText(
+                            text:
+                                'Mountain hikes give you an incredible sense of freedom along with endurance test',
+                            color: AppColors.textColor2,
+                            size: 15,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 30,
+                        ),
+                        ResponsiveButton(
+                          width: 80,
+                        ),
+                      ],
+                    ),
+                    Column(
+                      children: List.generate(3, (indexDots) {
+                        return Container(
+                          margin: const EdgeInsets.only(bottom: 2),
+                          width: 8,
+                          height: index == indexDots ? 25 : 8,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(8),
+                            color: index == indexDots
+                                ? AppColors.mainColor
+                                : AppColors.mainColor.withOpacity(0.5),
+                          ),
+                        );
+                      }),
+                    ),
+                  ],
                 ),
               ),
             );
